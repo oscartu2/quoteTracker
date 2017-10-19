26abf01242aff2e90405fa5207314b5e9ef6aa69
Oscar Tu <o.car.w2@gmail.com>
2017-10-19 13:40:07 -0700

    Add current price without formatting and fix bugs

--- Stock.py
+++ Stock.py
@@ -1,19 +1,22 @@
 from lxml import html
 import requests
-import time
 
 
 class Stock():
 	
-	def __init__(self, symbol, interval: float):
+	def __init__(self, symbol, previous_price, current_price):
 
-		self.previous_price = scrape(symbol)
-		time.sleep(interval)
-		self.current_price = scrape(symbol)
+		self.previous_price = previous_price
+		self.current_price = current_price
+		self.symbol = symbol
 		
-	def scrape(symbol):
-		page = requests.get('https://finance.yahoo.com/quote/' + str(symbol))
-		tree = html.formstring(page.content)
+	def scrape(self):
+		page = requests.get('https://finance.yahoo.com/quote/' + str(self.symbol))
+		tree = html.fromstring(page.content)
+
+		# Probably the current_price. Will need to debug
+		self.current_price = tree.xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]')
+		print(self.current_price)
 
 	def get_current_price(self):
 		return self.current_price
--- quoteTracker.py
+++ quoteTracker.py
@@ -1,16 +1,13 @@
-import Tkinter
-import tkMessageBox
+#import Tkinter
+#import tkMessageBox
+import Stock
+import time
 
 
 def refresh(interval):
 	# Updates the stocks and direction of the pricing with red/green arrows
 	# for every specified interval of time
 	print("test")
-
-
-def search():
-	# Automatically webscrape stock information 
-	# tkMessageBox.showinfo("Hello Python", "Hello World")
 
 def draw():
 	# Draw the GUI?
@@ -23,12 +20,16 @@ def update_buttons():
 	print("update btutons")
 
 def main():
+	flag = False
 	ticket_symbols = input("Please enter the list of ticket symbols separated by a comma that you want to track: ")
-	interval = input("Please enter the time interval (in seconds) you wish to check/update the stock: ")
+	interval = int(input("Please enter the time interval (in seconds) you wish to check/update the stock: "))
 	ts = ticket_symbols.strip().split(",")
 	ts_dict = {}
 	for symbol in ts:
-			stock = Stock(symbol, float(interval))
+			stock = Stock.Stock(symbol, 0.0, 0.0)
+			stock.scrape()
+			time.sleep(interval)
+			stock.update()
 			ts_dict[symbol] = stock
 			delta = stock.get_delta()
 			# Add onto GUI: Stock symbol on one side, and delta with red down/green up arrow on other side
@@ -37,7 +38,9 @@ def main():
 		for symbol in ts_dict:
 			ts_dict[symbol].update()
 			update_buttons() # If prev>curr show red, if prev<curr show green, if prev=curr show = sign
-	
+		if (flag):
+			break
+	print("Program Finished")
 	#draw()
 	#search()
 '''
@@ -52,5 +55,5 @@ B.pack()
 top.mainloop()
 '''
 
-#if __name__ == "__main__":
-	#main()
+if __name__ == "__main__":
+	main()
